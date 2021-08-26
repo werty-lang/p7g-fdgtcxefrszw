1330c419ab690f2e0f4f278968d2e17968910f72
werty-lang <84279588+werty-lang@users.noreply.github.com>
2021-08-26 19:12:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup(){
+    canvas= createCanvas(300,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Q397UzhaA/model.json',modelLoaded);
+
+}
+function draw(){
+image(video,0,0,300,300);
+classifier.classify(video, gotResult);
+
+
+}
+
+function modelLoaded(){
+    console.log('Model loaded')
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+
+    }  else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+
+    }
+}
